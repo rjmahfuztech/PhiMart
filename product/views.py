df3e6b9d0de231823a1d23526075c19945c7183a
rjmahfuztech <rjmahfuztech@gmail.com>
2025-02-26 14:21:29 +0600

    removed genericAPIView

--- product/views.py
+++ product/views.py
@@ -11,24 +11,6 @@ from rest_framework.viewsets import ModelViewSet
 
 # Create your views here.
 
-class ProductList(ListCreateAPIView):
-    queryset = Product.objects.select_related('category').all()
-    serializer_class = ProductSerializers
-
-
-class ProductDetails(RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializers
-    lookup_field = 'id'
-
-    # for specif customization if needed
-    # def delete(self, request, id):
-    #     product = get_object_or_404(Product, pk=id)
-    #     if product.stock > 5:
-    #         return Response({'message': 'Product with stock more then 5 can not be deleted.'})
-    #     product.delete()
-    #     return Response(status=status.HTTP_204_NO_CONTENT)
-
 '''All in one using (ModelViewSet) [create, read, update, delete]'''
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
@@ -42,15 +24,6 @@ class ProductViewSet(ModelViewSet):
         self.perform_destroy(product)
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-
-class CategoryList(ListCreateAPIView):
-    queryset = Category.objects.annotate(product_count=Count('products')).all()
-    serializer_class = CategorySerializers
-
-    
-class CategoryDetails(RetrieveUpdateDestroyAPIView):
-    queryset = Category.objects.annotate(product_count=Count('products')).all()
-    serializer_class = CategorySerializers
 
 '''All in one using (ModelViewSet) [create, read, update, delete]'''
 class CategoryViewSet(ModelViewSet):
