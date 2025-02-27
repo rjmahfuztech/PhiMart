f3ad769f19d801eaedf2e799c41389ac2710086e
rjmahfuztech <rjmahfuztech@gmail.com>
2025-02-27 21:11:43 +0600

    cartItem nested API created

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from product.views import ProductViewSet, CategoryViewSet, ReviewViewSet
-from order.views import CartViewSet
+from order.views import CartViewSet, CartItemViewSet
 from rest_framework_nested import routers
 
 
@@ -13,8 +13,11 @@ router.register('carts', CartViewSet, basename='carts')
 
 product_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
 product_router.register('reviews', ReviewViewSet, basename='product-review')
+cart_router = routers.NestedDefaultRouter(router, 'carts', lookup='cart')
+cart_router.register('items', CartItemViewSet, basename='cart-item')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('', include(product_router.urls))
+    path('', include(product_router.urls)),
+    path('', include(cart_router.urls))
 ]
--- order/views.py
+++ order/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
-from rest_framework.viewsets import GenericViewSet
-from order.serializers import CartSerializer
-from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
+from order.serializers import CartSerializer, CartItemSerializer
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
 from order.models import Cart, CartItem
 
 # Create your views here.
-class CartViewSet(CreateModelMixin, RetrieveModelMixin, GenericViewSet):
-    # queryset = Cart.objects.all()
+class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericViewSet):
     serializer_class = CartSerializer
 
     def get_queryset(self):
         return Cart.objects.prefetch_related('items__product').all()
+    
+class CartItemViewSet(ModelViewSet):
+    serializer_class = CartItemSerializer
+
+    def get_queryset(self):
+        return CartItem.objects.filter(cart_id=self.kwargs['cart_pk'])
