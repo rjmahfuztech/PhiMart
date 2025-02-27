abde2af08fdde1e164d5901c25b45b5dfe5f20b9
rjmahfuztech <rjmahfuztech@gmail.com>
2025-02-27 14:30:17 +0600

    secured cart id in cartModel

--- order/models.py
+++ order/models.py
@@ -1,18 +1,23 @@
 from django.db import models
 from users.models import User
 from product.models import Product
+from uuid import uuid4
 
 class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='cart')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'Cart of {self.user.username}'
+        return f'Cart of {self.user.first_name}'
     
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='cart_items')
     quantity = models.PositiveIntegerField()
+
+    class Meta:
+        unique_together = [['cart', 'product']]
 
     def __str__(self):
         return f'{self.quantity} x {self.product.name}'
@@ -34,7 +39,7 @@ class Order(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'Order Id {self.id} by {self.user.username}'
+        return f'Order Id {self.id} by {self.user.first_name}'
     
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
