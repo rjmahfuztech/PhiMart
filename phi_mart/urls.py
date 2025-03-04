40639c5c707ba07f45eef02c33adc0d31d740b4d
rjmahfuztech <rjmahfuztech@gmail.com>
2025-03-04 22:42:23 +0600

    permission added for product and category

--- phi_mart/urls.py
+++ phi_mart/urls.py
@@ -6,7 +6,7 @@ from .views import api_root_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', api_root_view),
-    path('api-auth/', include('rest_framework.urls')),
+    # path('api-auth/', include('rest_framework.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('api/v1/', include('api.urls'), name='api-root')
--- product/views.py
+++ product/views.py
@@ -8,6 +8,8 @@ from django_filters.rest_framework import DjangoFilterBackend
 from product.filters import ProductFilter
 from rest_framework.filters import SearchFilter, OrderingFilter
 from product.paginations import DefaultPagination
+# from rest_framework.permissions import IsAdminUser, AllowAny
+from api.permissions import IsAdminOrReadOnly
 
 # Create your views here.
 
@@ -21,6 +23,14 @@ class ProductViewSet(ModelViewSet):
     pagination_class = DefaultPagination
     search_fields = ['name', 'description']
     ordering_fields = ['price', 'updated_at']
+    # permission_classes = [IsAdminUser]
+    permission_classes = [IsAdminOrReadOnly]
+
+
+    # def get_permissions(self):
+    #     if self.request.method == 'GET':
+    #         return [AllowAny()]
+    #     return [IsAdminUser()]
     
 
 
@@ -45,6 +55,7 @@ class ProductViewSet(ModelViewSet):
 class CategoryViewSet(ModelViewSet):
     queryset = Category.objects.annotate(product_count=Count('products')).all()
     serializer_class = CategorySerializer
+    permission_classes = [IsAdminOrReadOnly]
 
 
 class ReviewViewSet(ModelViewSet):
