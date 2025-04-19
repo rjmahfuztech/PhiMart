f7f10ea9cb45a8a520be4132a5cede6a04ffe852
rjmahfuztech <rjmahfuztech@gmail.com>
2025-04-19 21:46:38 +0600

    ProductImage model register

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from product.models import Category, Product, Review
+from product.models import Category, Product, Review, ProductImage
 
 # Register your models here.
-admin.site.register(Category)
 admin.site.register(Product)
+admin.site.register(ProductImage)
+admin.site.register(Category)
 admin.site.register(Review)
