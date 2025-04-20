39730ba70b5b920a78083d969a471a1b5509c975
rjmahfuztech <rjmahfuztech@gmail.com>
2025-04-21 02:37:20 +0600

    phimart front-end live link added on cors_allowed_origins

--- phi_mart/settings.py
+++ phi_mart/settings.py
@@ -77,7 +77,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'phi_mart.wsgi.app'
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'https://phimart-ecommerce.vercel.app'
 ]
 
 INTERNAL_IPS = [
